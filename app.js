2ef7aa8c72042fc69280cf7cf062cd0b0cb78082
KATIE BERRY <KATIEBERRYM@GMAIL.COM>
2020-10-12 18:13:15 -0700

    added getrandompoke function

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 // import functions and grab DOM elements
-// import { pokeData } from './Data/poke-data';
+import { pokeData } from './Data/poke-data';
 
 const encountered = document.querySelector('#encountered');
 const caught = document.querySelector('#caught');
 const images = document.querySelectorAll('label > img');
 const radios = document.querySelectorAll('input');
-console.log(radios);
 
 // initialize state
 let plays = 0;
 const encounteredPoke = pokeData.slice();
-const capturedPoke = [];
+let capturedPoke = [];
 
 // set event listeners to update state and DOM
 
 function getRandomPoke(someArray) {
-    const index = Math.floor(Math.random()) * someArray.length;
+    const index = Math.floor(Math.random() * someArray.length);
+    return someArray[index];
 }
+
+let pokeOne = getRandomPoke(pokeData); 
+let pokeTwo = getRandomPoke(pokeData);
+let pokeThree = getRandomPoke(pokeData);
+
+while (pokeOne.id === pokeTwo.id) {
+    pokeOne = getRandomPoke(pokeData);
+}
+
+while (pokeTwo.id === pokeThree.id) {
+    pokeTwo = getRandomPoke(pokeData);
+}
+
+while (pokeThree.id === pokeOne.id) {
+    pokeTwo = getRandomPoke(pokeData);
+}
+console.log(pokeOne);
