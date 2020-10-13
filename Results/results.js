2d2df6c0d4ea66f6e4c8a76000d5197692fe43c7
KATIE BERRY <KATIEBERRYM@GMAIL.COM>
2020-10-12 17:10:16 -0700

    added more css and results folder

---
+++ Results/results.js
@@ -0,0 +1,8 @@
+// import functions and grab DOM elements
+// import { pokeData } from './Data/poke-data';
+
+const clearButton = document.querySelector('button');
+
+console.log(clearButton);
+
+
--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 // import functions and grab DOM elements
+// import { pokeData } from './Data/poke-data';
+
+const encountered = document.querySelector('#encountered');
+const caught = document.querySelector('#caught');
+const images = document.querySelectorAll('label > img');
+const radios = document.querySelectorAll('input');
+console.log(radios);
 
 // initialize state
+let plays = 0;
+const encounteredPoke = pokeData.slice();
+const capturedPoke = [];
 
 // set event listeners to update state and DOM
+
+function getRandomPoke(someArray) {
+    const index = Math.floor(Math.random()) * someArray.length;
+}
